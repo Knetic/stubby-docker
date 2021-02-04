1375505154cc776b6902c2b0ab97e1a873bd7876
George Lester <glester491@gmail.com>
2021-02-03 18:40:05 -0800

    made build use net=host

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-docker build -t dot-stubby ./stubby
+docker build \
+	--network=host \
+	-t dot-stubby \
+	./stubby
 docker tag dot-stubby:latest dot-stubby:local
 
-docker build -t dot-unbound ./unbound
+docker build \
+	--network=host \
+	-t dot-unbound \
+	./unbound
 docker tag dot-unbound:latest dot-unbound:local
